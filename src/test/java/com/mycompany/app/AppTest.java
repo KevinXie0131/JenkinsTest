83dfb48d1595a875eb9ff92dcb010b6908bf7c13
Kevin Xie <zhibing.x@gmail.com>
2019-11-27 17:21:50 -0500

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,12 +40,6 @@ public class AppTest
         }
     }
 
-    @Test
-    public void testAppMain1()
-    {
-       assertEquals("Hello World!@@@@" + System.getProperty("line.separator"), outContent.toString());
-    }
-    
     @After
     public void cleanUpStreams() {
         System.setOut(null);
